c60246e44a5e65a6ecf4ba2c4d38ef25fecf0c1a
origin <thuynguyenminh0303@gmail.com>
2023-03-08 23:57:40 +0700

    tao routing + components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CategoryComponent } from './components/category/category.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { IndexComponent } from './components/index/index.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IndexComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'category', component: CategoryComponent},
+  {path:'login', component: LoginComponent},
+  {path: 'productDetail/1', component: ProductDetailComponent},
+  {path: 'register', component: RegisterComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { CategoryComponent } from './components/category/category.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { IndexComponent } from './components/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    LoginComponent,
+    RegisterComponent,
+    CategoryComponent,
+    ProductDetailComponent,
+    IndexComponent,
+
   ],
   imports: [
     BrowserModule,
